20dca1392dfa252e5a5d4d933c151ba077d834c9
Frank Wang <frankwang0098@gmail.com>
2025-03-18 13:04:35 -0700

    Added code to apply force to ball when shot. Will refactor into the ball controller

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,10 +4,23 @@ public class InputManager : MonoBehaviour
 {
     public Transform shooterPivot;      
     public GameObject ball;      
-    public float shootForce = 10f;      
+    public float shootForce = -1f;      
 
     void Update()
     {
-        
+        //on left mouse button clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            //spawn a new Ball at the pivot’s position and rotation
+            GameObject newBall = Instantiate(ball, shooterPivot.position, shooterPivot.rotation);
+
+            Rigidbody2D rb = newBall.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                //apply force
+                rb.AddForce(shooterPivot.up * shootForce, ForceMode2D.Impulse);
+            }
+        }
     }
 }
