725c106255104a5e5b7cfa2f387452af43a3b7a1
Frank Wang <frankwang0098@gmail.com>
2025-03-25 16:57:09 -0700

    Added code for the pause settings menu button. The pause menu also has working play and return to menu buttons

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject settingsMenu;
+
+    private bool isSettingsMenuActive;
+
+    private void Awake()
+    {
+        DisableSettingsMenu();
+    }
+
+    public void ToggleSettingsMenu()
+    {
+        if (isSettingsMenuActive)
+        {
+            DisableSettingsMenu();
+        }
+        else
+        {
+            EnableSettingsMenu();
+        }
+    }
+
+    public void EnableSettingsMenu()
+    {
+        Time.timeScale = 0f;
+        settingsMenu.SetActive(true);
+        isSettingsMenuActive = true;
+    }
+    public void DisableSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        settingsMenu.SetActive(false);
+        isSettingsMenuActive = false;
+    }
+
+    public void ReturnMainMenu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+}
