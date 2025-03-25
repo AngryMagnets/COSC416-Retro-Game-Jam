9854b437c812e694c413de58e143ce6d8eeb8d8c
AngryMagnets <32856537+AngryMagnets@users.noreply.github.com>
2025-03-25 13:26:17 -0700

    Ball UI Complete
    Needs testing with game manager

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BallManager : MonoBehaviour
 {
@@ -8,11 +10,21 @@ public class BallManager : MonoBehaviour
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private GameObject ball;
     [SerializeField] private int startingBalls;
+
+    [SerializeField] TextMeshProUGUI ballText;
+
+    [SerializeField]
+    private Ease AnimationCurve;
+    [SerializeField]
+    private float duration;
+
+
     private int numBalls;
     private List<GameObject> ballList;
 
     private void Start()
     {
+        numBalls = 0;
         ballList = new List<GameObject>();
     }
     void Awake()
@@ -28,8 +40,11 @@ public class BallManager : MonoBehaviour
 
     public void RemoveBall()
     {
-
+        ballList[0].transform.DOLocalMoveY(-10, duration).SetEase(AnimationCurve);
         numBalls--;
+        GameObject temp = ballList[0];  
+        ballList.RemoveAt(0);
+        Destroy(temp);
     }
 
     private IEnumerator<WaitForSeconds> AddBalls (int ballCount)
@@ -38,8 +53,9 @@ public class BallManager : MonoBehaviour
         {
             yield return new WaitForSeconds(0.3f);
             ballList.Add(Instantiate(ball, spawnPoint));
+            numBalls++;
             StartCoroutine(AddBalls(ballCount - 1));
         }
-        yield return null;
+        yield return new WaitForSeconds(0f);
     }
 }
