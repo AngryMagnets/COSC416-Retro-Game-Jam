69df866ae9e7ec44871eb3baef01b411816b799f
Frank Wang <frankwang0098@gmail.com>
2025-03-31 17:05:33 -0700

    Added button click sound to all buttons

--- Assets/Scripts/SoundHandler/ButtonClickSound.cs
+++ Assets/Scripts/SoundHandler/ButtonClickSound.cs
@@ -1,21 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SoundHandler : MonoBehaviour
+public class ButtonClickSound : MonoBehaviour
 {
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip buttonClickSound;
-
-    void Awake()
-    {
-
-    }
 
     public void PlayClickSound()
     {
         if (audioSource != null && buttonClickSound != null)
         {
-            audioSource.PlayOneShot(buttonClickSound);
+            audioSource.PlayOneShot(buttonClickSound, 5f);
         }
     }
 }
